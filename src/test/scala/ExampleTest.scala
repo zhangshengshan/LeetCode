75ebd88efc98a1083707f2653c64f068d6bf0480
zhangshengshan <zhangshengshan@youzan.com>
2018-03-05 13:08:46 +0800

    update

--- src/test/scala/ExampleTest.scala
+++ src/test/scala/ExampleTest.scala
@@ -1,9 +1,8 @@
-import org.scalatest.Assertions._
-import collection.mutable.Stack
+//import org.scalatest.Assertions._
+//import collection.mutable.Stack
 import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
-
   "TwoSum brute force" should "TwoSum.twoSum" in {
     val a = TwoSum.twoSum(Array(1,2,3,4),5)
     List(Array(2,1), Array(1,2), Array(3,0), Array(0,3)) should contain (a)
