34ac7e8443c0cad056f9f36c75d1b3c3c4383870
zhangshengshan <zhangshengshan@youzan.com>
2020-02-27 15:54:11 +0800

    update

--- src/main/scala/TwoSum.scala
+++ src/main/scala/TwoSum.scala
@@ -1,37 +1,29 @@
-object TwoSum {
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//
+// 示例:
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//
+// Related Topics 数组 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+object Solution {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    val tmpList = for ( i <- 0 until nums.length -1 ; j <- i + 1 until nums.length if nums(i) + nums(j) == target) yield Array(i,j)
-    tmpList.headOption.getOrElse(Array())
+    0 to nums.length -1 foreach { i =>
+      if (nums.contains(target - nums(i))) return Array(i, nums.indexOf(target-nums(i)))
+    }
+    null
   }
 
-  def twoSum1(nums: Array[Int], target: Int): Array[Int] = {
-    val map = new scala.collection.mutable.HashMap[Int,Int]
-    for ( i <- 0 until nums.length ) {
-      map(nums(i)) = i
-    }
-    for ( i <- 0 until nums.length ) {
-      if ( map.contains( target - nums(i))){
-        return Array(i, map(target - nums(i)))
-      }
-    }
-    return Array()
-  }
-
-  def twoSum2(nums: Array[Int], target: Int): Array[Int] = {
-    val map = new scala.collection.mutable.HashMap[Int,Int]
-    for ( i <- 0 until nums.length ) {
-      if ( map.contains( target - nums(i))){
-        return Array(map(target - nums(i)),i)
-      }else{
-        map(nums(i)) = i
-      }
-    }
-    return Array()
-  }
   def main(args: Array[String]): Unit = {
-    val aa = twoSum2(Array(1,2,3,4),3)
-    println(aa.length)
-    println("==============================")
-    aa.toList.foreach(println)
+    val ints: Array[Int] = twoSum(Array(1, 2, 3), 9)
+    ints.foreach(println)
   }
 }
+//leetcode submit region end(Prohibit modification and deletion)
