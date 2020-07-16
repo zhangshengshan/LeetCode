d5177190dd0bc46fd32e8b07308b4a79b37415dd
zhangshengshan <zhangshengshan@youzan.com>
2020-07-16 16:48:07 +0800

    tmp ci

--- src/main/scala/leetcode/editor/cn/[1184]公交站间的距离.scala
+++ src/main/scala/leetcode/editor/cn/[1184]公交站间的距离.scala
@@ -1,36 +1,17 @@
 //环形公交路线上有 n 个站，按次序从 0 到 n - 1 进行编号。我们已知每一对相邻公交站之间的距离，distance[i] 表示编号为 i 的车站和编号
 //为 (i + 1) % n 的车站之间的距离。 
-//
 // 环线上的公交车都可以按顺时针和逆时针的方向行驶。 
-//
 // 返回乘客从出发点 start 到目的地 destination 之间的最短距离。 
-//
-// 
-//
 // 示例 1： 
-//
-// 
-//
 // 输入：distance = [1,2,3,4], start = 0, destination = 1
 //输出：1
 //解释：公交站 0 和 1 之间的距离是 1 或 9，最小值是 1。 
-//
-// 
-//
 // 示例 2： 
-//
-// 
-//
 // 输入：distance = [1,2,3,4], start = 0, destination = 2
 //输出：3
 //解释：公交站 0 和 2 之间的距离是 3 或 7，最小值是 3。
 // 
-//
-// 
-//
 // 示例 3： 
-//
-// 
 //
 // 输入：distance = [1,2,3,4], start = 0, destination = 3
 //输出：4
@@ -50,10 +31,112 @@
 // Related Topics 数组
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //leetcode submit region begin(Prohibit modification and deletion)
 object Solution {
-    def distanceBetweenBusStops(distance: Array[Int], start: Int, destination: Int): Int = {
-
-    }
+  def distanceBetweenBusStops(distance: Array[Int], start: Int, destination: Int): Int = {
+    val totalSum: Int = distance.sum
+    val sum: Int = distance.slice(math.min(start, destination), math.max(start, destination)).sum
+    math.min(sum, totalSum - sum)
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
